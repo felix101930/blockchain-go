571fbc1925e5478d1fe901485668ab237c2504fa
felix-717 <vaghs622@gmail.com>
2025-02-25 18:52:22 -0700

    updates block and proof of work

--- main.go
+++ main.go
@@ -1,69 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+
+	"github.com/felix101930/blockchain-go/blockchain"
 )
 
-type BlockChain struct{
-
-	blocks []*Block
-
-}
-type Block struct{
-
-	Hash []byte
-	Data []byte
-	PrevHash []byte
-
-}
-
-func (chain *BlockChain) AddBlock(data string){
-
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.PrevHash)
-	chain.blocks = append(chain.blocks, new)
-
-}
-
-// create first block 
-func Genesis() *Block {
-	return CreateBlock("Genesis",[]byte{})
-}
-
-
-func InitBlockChain() *BlockChain{
-
-	return &BlockChain{[]*Block{Genesis()}}
-
-}
-
-func (b *Block) DeriveHash() {
-
-	info :=bytes.Join([][]byte{b.Data,b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-
-
-}
 
 func main() {
 
-	chain := InitBlockChain()
+	chain := blockchain.InitBlockChain()
 
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
 
-	for _, block :=range chain.blocks {
+	for _, block :=range chain.Blocks {
 
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data : %s\n", block.Data)
