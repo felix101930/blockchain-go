05bdae6b70fb9f4268678123f48fe58866621012
felix-717 <vaghs622@gmail.com>
2025-02-25 17:32:06 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/felix101930/blockchain-go
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+type Block struct{
+
+	Hash []byte
+	Data []byte
+	PrevHash []byte
+
+}
+
+func main() {
+
+	
+
+
+}
