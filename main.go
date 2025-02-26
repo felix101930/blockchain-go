07d0ade5f0167b042b998da20053d1d844e86166
felix-717 <vaghs622@gmail.com>
2025-02-25 18:28:36 -0700

    outputs blockchain

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
 
 type BlockChain struct{
@@ -61,6 +62,13 @@ func main() {
 	chain.AddBlock("First Block after Genesis")
 	chain.AddBlock("Second Block after Genesis")
 	chain.AddBlock("Third Block after Genesis")
+
+	for _, block :=range chain.blocks {
+
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data : %s\n", block.Data)
+		fmt.Printf("Hash : %x\n", block.Hash)
+	}
 
 	
 
